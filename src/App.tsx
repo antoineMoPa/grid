0933c8830cd26585ee37a43d8d068a9df06518e0
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2024-06-14 01:55:44 -0400

    Remove outdated shortcut

--- src/App.tsx
+++ src/App.tsx
@@ -263,7 +263,7 @@ function App() {
                 </div>
                 <div className="leave-trail mt-4">
                     <p>Use <span className="keyboard-shortcut">Shift + Arrows</span> to auto sweep.</p>
-                    <p>Trail Wall Size <span className="keyboard-shortcut">T</span></p>
+                    <p>Trail Wall Size</p>
                     <div>
                         <ButtonGroup size="sm">
                             { leaveTrailOptions.map((value) => (
